3f5fe50f8fa4631f5d4e330845f9bd5189098fa5
Chuck <c.costa.gdev@gmail.com>
2022-12-26 18:32:48 +0000

    MediaLoadScene Bones
    Needs to be made pretty now

---
+++ Assets/Scripts/MediaLoadManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Video;
+
+public class MediaLoadManager : MonoBehaviour
+{
+    public string texture_url;
+    public string video_url;
+
+
+    public RawImage raw;
+    public VideoPlayer vp;
+    //public RawImage video;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartButton()
+    {
+        StartCoroutine(GetTexture());
+
+        vp.url = video_url;
+        vp.Prepare();
+        vp.prepareCompleted += PlayVideo;
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://codeshare.co.uk/media/1068/search-best-free-stock-images-one-place.jpg");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            raw.texture = myTexture;
+        }
+    }
+
+    void PlayVideo(VideoPlayer source)
+    {
+        source.Play();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
