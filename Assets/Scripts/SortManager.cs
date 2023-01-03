e78f131b067a479be5bae294d8f1566b301bbfe8
Chuck <c.costa.gdev@gmail.com>
2023-01-03 01:26:20 +0000

    Floating

--- Assets/Scripts/SortManager.cs
+++ Assets/Scripts/SortManager.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Mathematics;
+using UnityEngine.UI;
+using TMPro;
 
 public class SortManager : MonoBehaviour
 {
     public GameObject cubePrefab;
-
+    public Transform floatingParent;
     public int size = 64;
-    public int distance = 1;
 
-    List<GameObject> cubeList;
+    public List<GameObject> sortedList;
+    List<GameObject> unsortedList;
 
     // Start is called before the first frame update
     void Start()
     {
-        cubeList = new List<GameObject>();
+        unsortedList = new List<GameObject>();
+        sortedList = new List<GameObject>();
         GenerateCubes();
     }
 
@@ -28,22 +30,47 @@ public class SortManager : MonoBehaviour
         for (int i = 0; i < sqr; i++)
             for (int j = 0; j < sqr; j++)
             {
+                int rnd = Random.Range(0, 3);
+
                 GameObject o = Instantiate(
                     cubePrefab,
                     this.transform.position,
                     Quaternion.identity,
                     this.transform
                 );
+                var im = o.GetComponent<Image>();
 
-                cubeList.Add(o);
+                switch (rnd)
+                {
+                    case 0:
+                        im.color = Color.red;
+                        break;
+                    case 1:
+                        im.color = Color.green;
+                        break;
+                    case 2:
+                        im.color = Color.blue;
+                        break;
+                }
+
+                unsortedList.Add(o);
+                o.GetComponentInChildren<TextMeshProUGUI>().text = unsortedList.Count.ToString();
             }
 
-        Debug.Log(cubeList.Count);
+        Debug.Log(unsortedList.Count);
     }
 
-    void MoveCube()
+    public void MoveCube()
     {
+        //Move it to the sortedList
+        GameObject o = unsortedList[0];
+        GameObject clone = Instantiate(o, o.transform.position, o.transform.rotation, floatingParent);
+        o.transform.parent = floatingParent;
+        sortedList.Add(o);
+        unsortedList.Remove(o);
 
+        //Give it it's new position
+        //On Arrival set the new parents
     }
 
     // Update is called once per frame
